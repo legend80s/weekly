eacd4119ff6af749db2c48c56e8a1c9d3491c966
legend80s <liuchzong@qq.com>
2026-02-27 16:03:26 +0800

    fix(readwise): cannot resolve title when title has no link

--- readwise-weekly/src/index.js
+++ readwise-weekly/src/index.js
@@ -17,7 +17,11 @@ const articles = $articles.map((group) => {
 
 	const subArticles = $subArticles.map((article) => {
 		const img = article.querySelector("img[src*=cover]");
-		const titleNode = article.querySelector("a[class*=titleLink_]");
+		const titleNode =
+			article.querySelector("a[class*=titleLink_]") ??
+			article.querySelector(
+				"table > tbody > tr > td > table > tbody > tr:nth-child(2)",
+			);
 
 		const maybeAuthorNode = titleNode?.closest("tr")?.nextElementSibling;
 		// 包含 docBody_ 的元素是摘要，不是作者信息
@@ -25,9 +29,18 @@ const articles = $articles.map((group) => {
 			? null
 			: maybeAuthorNode;
 
+		const title = titleNode?.textContent;
+		const url = titleNode?.getAttribute("href");
+
+		if (!title) {
+			console.error("titleNode:", titleNode);
+			console.error("article:", article);
+			throw new Error(`Missing title (${title}) for article`);
+		}
+
 		return {
-			title: titleNode?.textContent,
-			url: titleNode?.getAttribute("href"),
+			title,
+			url,
 			img: img?.getAttribute("src"),
 			// @ts-expect-error innerText
 			summary: article.querySelector("[class*=docBody_]")?.innerText,
@@ -53,8 +66,10 @@ const md = articles
 				const source = [sub.author, sub.media && `**${sub.media}**`]
 					.filter(Boolean)
 					.join(" | ");
+				const title = sub.url ? `[${sub.title}](${sub.url})` : sub.title;
+
 				return `
-### ${i + 1}.${j + 1} [${sub.title}](${sub.url})
+### ${i + 1}.${j + 1} ${title}
 
 ![${sub.title}](${sub.img})
 
@@ -72,3 +87,6 @@ ${articlesMd}`;
 	.join("\n");
 
 console.log("markdown:", md);
+
+// @ts-expect-error
+copy(md);
